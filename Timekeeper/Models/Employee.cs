2565def61c4d12dc3f1606104fc5a0195536c5b7
Ridon Borlaza <ridon_borlaza@yahoo.com>
2026-03-28 20:43:00 +0800

    Completed Employee registration module in UI

--- Timekeeper/Models/Employee.cs
+++ Timekeeper/Models/Employee.cs
@@ -1,13 +1,18 @@
-﻿namespace Timekeeper.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Timekeeper.Models
 {
     public class Employee
     {
         public int EmployeeId { get; set; }
 
+        [Required(ErrorMessage = "First name is required.")]
         public string FirstName { get; set; }
 
+        [Required(ErrorMessage = "Last name is required.")]
         public string LastName { get; set; }
 
+        [Required(ErrorMessage = "Gender is required.")]
         public string Gender { get; set; }
 
         public DateTime DateHired { get; set; }
