f828fa794d3e1cba4428c220060ccb0a0652a18a
Ridon Borlaza <ridon_borlaza@yahoo.com>
2026-03-28 14:08:50 +0800

    Added Employee, TimekeepingTransaction, and TransactionType classes.

---
+++ Timekeeper/Models/TransactionType.cs
@@ -0,0 +1,11 @@
+﻿namespace Timekeeper.Models
+{
+    public class TransactionType
+    {
+        public int TransactionTypeId { get; set; }
+
+        public string TransactionTypeName { get; set; }
+
+        public List<TimekeepingTransaction> TimekeepingTransaction { get; set; }
+    }
+}
