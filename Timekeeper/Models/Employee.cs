ce91b73e7ae137b7f003bdd15638e7ca2a0e38e5
Ridon Borlaza <ridon_borlaza@yahoo.com>
2026-03-29 13:09:53 +0800

    Added notes in models and local datetime struct in TimekeepingTransaction

--- Timekeeper/Models/Employee.cs
+++ Timekeeper/Models/Employee.cs
@@ -1,16 +1,21 @@
-﻿namespace Timekeeper.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Timekeeper.Models
 {
     public class Employee
     {
         public int EmployeeId { get; set; }
 
+        [Required(ErrorMessage = "First name is required.")]
         public string FirstName { get; set; }
 
+        [Required(ErrorMessage = "Last name is required.")]
         public string LastName { get; set; }
 
-        public string Gender { get; set; }
+        [Required(ErrorMessage = "Gender is required.")]
+        public string Gender { get; set; } // Can be an enum. Will use string for simplicity.
 
-        public DateOnly DateHired { get; set; }
+        public DateTime DateHired { get; set; }
 
         public List<TimekeepingTransaction> TimeKeepingTransactions { get; } = new();
     }
