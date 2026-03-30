7bea8184c7701b5a69e271b06e0e5271ae466ec2
Ridon Borlaza <ridon_borlaza@yahoo.com>
2026-03-30 08:43:16 +0800

    Created publishing files

---
+++ Timekeeper/Migrations/20260330002734_ProdCreate.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Timekeeper.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProdCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
